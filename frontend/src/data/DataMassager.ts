98a59f2567cdb46a1153cf5da71bc9f748a4eba9
Zahoor Mohamed <zahoor@zahoor.in>
2023-02-07 19:20:20 +0400

    Display errors seperately

--- frontend/src/data/DataMassager.ts
+++ frontend/src/data/DataMassager.ts
@@ -19,7 +19,7 @@ export function appendOtherGroup(list: NamedCount[] | undefined): [NamedCount[],
   list.forEach((item) => {
     if (item.name === 'tmp'  || item.name === '') {
       unknownItemCount +=  item.count
-    } else if (filteredList.length > 9) {
+    } else if (filteredList.length > 20) {
       otherItem.count += item.count
     } else {
       filteredList.push(item)
@@ -31,4 +31,52 @@ export function appendOtherGroup(list: NamedCount[] | undefined): [NamedCount[],
   }
 
   return [filteredList, unknownItemCount];
+}
+
+export function getClients(list: NamedCount[] | undefined): [NamedCount[], number] {
+  let unknownItemCount = 0
+
+  if (!list) {
+    return [[], unknownItemCount];
+  }
+
+  const otherItem: NamedCount = {
+    name: 'Other',
+    count: 0
+  };
+
+  const filteredList: NamedCount[] = []
+  list.forEach((item) => {
+    if (item.name.includes('_')) {
+      return
+    }
+    if (item.name === 'tmp'  || item.name === '') {
+      unknownItemCount +=  item.count
+    } else if (filteredList.length > 10) {
+      otherItem.count += item.count
+    } else {
+      filteredList.push(item)
+    }
+  })
+
+  if (otherItem.count > 0) {
+    filteredList.push(otherItem)
+  }
+
+  return [filteredList, unknownItemCount];
+}
+
+export function getErrors(list: NamedCount[] | undefined): NamedCount[] {
+  if (!list) {
+    return [];
+  }
+
+  const filteredList: NamedCount[] = []
+  list.forEach((item) => {
+    if (item.name.includes('_')) {
+      filteredList.push(item)
+    }
+  })
+
+  return filteredList
 }
