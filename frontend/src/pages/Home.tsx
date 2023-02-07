6a7ecef358c3a131fb5d8ba26cc7dd152400a4bf
Zahoor Mohamed <zahoor@zahoor.in>
2023-02-07 19:31:46 +0400

    properly reduce size of clients display

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -151,7 +151,7 @@ function Home() {
               <BarChart
                 data={barChartData}
                 layout="vertical"
-                margin={{ left: 400, right: 30 }}
+                margin={{ left: 600, right: 30 }}
                 onClick={data.versions.length ? onVersionClicked : onClientClicked}
               >
                 <XAxis type="number" hide stroke={color} />
@@ -177,7 +177,7 @@ function Home() {
               <BarChart
                   data={data.errors}
                   layout="vertical"
-                  margin={{ left: 60, right: 30 }}
+                  margin={{ left: 400, right: 30 }}
                   onClick={data.versions.length ? onVersionClicked : onClientClicked}
               >
                 <XAxis type="number" hide stroke={color} />
